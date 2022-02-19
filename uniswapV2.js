59d08a0d134328749eb959aee7d0c95d0ba8559c
ali-ahmed-05 <aliahmed.pluton@gmail.com>
2022-02-19 11:06:14 +0500

    moralis

--- uniswapV2.js
+++ uniswapV2.js
@@ -9,8 +9,8 @@ const DAI = new uniswap.Token(chainId, tokenAddress, decimals);
 
 const USDC = new uniswap.Token(
     uniswap.ChainId.MAINNET,
-    "0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984",  //USDC 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
-    18
+    "0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48",  //USDC 0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48
+    6
   );
 
 // UNI token address 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984
